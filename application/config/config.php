473322d0dd16d8a39e51ae552299d0a68a759a39
shohihul <shohihul29maksud@gmail.com>
2021-03-14 22:13:15 +0700

    migration

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') or exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://barbershop.9cahayagroup.com/';
+$config['base_url'] = 'http://karisma-pos.funtasticjfc.com/';
 // $config['base_url'] = '';
 
 /*
